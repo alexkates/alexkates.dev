f7a28c184d2afca92e4f7b26aac2501ff34e5ad6
Alex Kates <alex.kates@croissant.com>
2023-12-22 06:22:09 -0500

    Add Radix UI packages and implement theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,7 +28,15 @@ export default function RootLayout({ children }: Props) {
           fontSans.variable
         )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ModeToggle />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
