42bbb872441ac2b6f35a1e284f98f695ba86c81b
Alex Kates <alex.kates@croissant.com>
2023-12-22 21:21:54 -0500

    Refactor blog post item component and remove prettier configuration file

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-  printWidth: 120,
-  plugins: ["prettier-plugin-organize-imports"],
-};
-
-module.exports = config;
--- src/components/blog-post-item.tsx
+++ src/components/blog-post-item.tsx
@@ -1,5 +1,6 @@
 import BlogPost from "@/types/BlogPost";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   post: BlogPost;
@@ -9,11 +10,19 @@ export default function BlogPostItem({ post }: Props) {
   return (
     <li className="flex flex-col items-center">
       <div className="flex flex-col items-center border border-transparent hover:border-primary p-2 rounded-lg transition ease-in-out duration-300">
-        <Image width={500} height={150} alt={post.title} src={post.coverImage.url} className="rounded-lg" />
-        <div className="flex justify-between w-full text-sm ">
-          <span>{new Date(post.publishedAt).toLocaleDateString()}</span>
-          <span>{post.views.toLocaleString()} views</span>
-        </div>
+        <Link href={`/blog/${post.slug}`}>
+          <Image
+            width={500}
+            height={150}
+            alt={post.title}
+            src={post.coverImage.url}
+            className="rounded-lg"
+          />
+          <div className="flex justify-between w-full text-sm ">
+            <span>{new Date(post.publishedAt).toLocaleDateString()}</span>
+            <span>{post.views.toLocaleString()} views</span>
+          </div>
+        </Link>
       </div>
     </li>
   );
