284cc29821439a9c4e5b25546cf268926b744b55
Alex Kates <alex.kates@croissant.com>
2023-12-27 08:43:56 -0500

    Add tailwindcss-animation-delay plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,31 @@
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export default function Home() {
+  const fadeIn = "animate-in fade-in duration-1000 fill-mode-both";
   return (
-    <section className="flex flex-col">
-      <div className="flex flex-row items-center text-2xl animate-in fade-in duration-300 delay-1000">
+    <article className="flex flex-col">
+      <section className={cn("flex flex-row items-center text-2xl", fadeIn, "animation-delay-100")}>
         <span className="mr-1 animate-wave">👋</span>
         <h1 className="font-medium tracking-tighter">Hi, I&apos;m Alex.</h1>
-      </div>
-      <p className="prose prose-neutral dark:prose-invert">
-        I&apos;m a{" "}
-        <Link href="https://leerob.io/blog/product-engineers" target="_blank">
-          product engineer
-        </Link>{" "}
-        and{" "}
-        <Link href="https://ammar-hakim.org/sj/pn/pn0/pn0-minimalism.html" target="_blank">
-          software minimalist
-        </Link>{" "}
-        currently working at{" "}
-        <Link href="https://croissant.com?utm_source=alexkates.dev" target="_blank">
-          Croissant
-        </Link>{" "}
-        as Director of Engineering.
-      </p>
-    </section>
+      </section>
+      <section className={cn("flex flex-row items-center text-2xl", fadeIn, "animation-delay-200")}>
+        <p className="prose prose-neutral dark:prose-invert">
+          I&apos;m a{" "}
+          <Link href="https://leerob.io/blog/product-engineers" target="_blank">
+            product engineer
+          </Link>{" "}
+          and{" "}
+          <Link href="https://ammar-hakim.org/sj/pn/pn0/pn0-minimalism.html" target="_blank">
+            software minimalist
+          </Link>{" "}
+          currently working at{" "}
+          <Link href="https://croissant.com?utm_source=alexkates.dev" target="_blank">
+            Croissant
+          </Link>{" "}
+          as Director of Engineering.
+        </p>
+      </section>
+    </article>
   );
 }
