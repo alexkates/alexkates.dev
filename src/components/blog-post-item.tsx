42a60dcad48a052a8fc4637792502aad91e9ca51
Alex Kates <alex.kates@croissant.com>
2023-12-27 09:29:46 -0500

    Add fadeIn animation to BlogPostItem component

--- src/components/blog-post-item.tsx
+++ src/components/blog-post-item.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import BlogPost from "@/types/BlogPost";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,9 +8,11 @@ type Props = {
 };
 
 export default function BlogPostItem({ post }: Props) {
+  const fadeIn = "animate-in fade-in duration-1000 fill-mode-both";
+
   return (
-    <li className="flex flex-col items-center rounded-lg transition-transform duration-300 ease-in-out hover:scale-105">
-      <Link href={`/blog/${post.slug}`}>
+    <li className={cn("flex flex-col items-center rounded-lg", fadeIn)}>
+      <Link href={`/blog/${post.slug}`} className="transition-transform duration-300 ease-in-out hover:scale-105">
         <Image width={500} height={150} alt={post.title} src={post.coverImage.url} className="rounded-lg" />
         <div className="flex w-full justify-between text-xs">
           <span>{new Date(post.publishedAt).toLocaleDateString()}</span>
