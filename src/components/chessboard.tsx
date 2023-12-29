a43a1274fb3f4c97ddb08dd7d8254e5408b7b4ed
Alex Kates <alex.kates@croissant.com>
2023-12-28 20:52:21 -0500

    Add chess.js library and implement chessboard functionality

--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -1,17 +1,51 @@
 "use client";
 
+import { Chess } from "chess.js";
 import dynamic from "next/dynamic";
+import { useState } from "react";
 
 const ChessboardJSX = dynamic(() => import("chessboardjsx"), {
   ssr: false,
 });
 
 function Chessboard() {
-  const calcWidth = ({ screenWidth, screenHeight }: { screenWidth: number; screenHeight: number }) => {
-    return Math.min(screenWidth, screenHeight) * 0.8;
+  const [chess] = useState(new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
+  const [fen, setFen] = useState(chess.fen());
+
+  type Move = {
+    from: string;
+    to: string;
+    promotion?: string;
   };
 
-  return <ChessboardJSX position="start" calcWidth={calcWidth} />;
+  const handleMove = (move: Move) => {
+    if (chess.move(move)) {
+      setTimeout(() => {
+        const moves = chess.moves();
+        if (moves.length > 0) {
+          const computerMove = moves[Math.floor(Math.random() * moves.length)];
+          chess.move(computerMove);
+          setFen(chess.fen());
+        }
+      }, 300);
+      setFen(chess.fen());
+    }
+  };
+
+  const calcWidth = ({ screenWidth, screenHeight }: { screenWidth: number; screenHeight: number }) => Math.min(screenWidth, screenHeight) * 0.8;
+  return (
+    <ChessboardJSX
+      position={fen}
+      calcWidth={calcWidth}
+      onDrop={(move) =>
+        handleMove({
+          from: move.sourceSquare,
+          to: move.targetSquare,
+          promotion: "q",
+        })
+      }
+    />
+  );
 }
 
 export default Chessboard;
