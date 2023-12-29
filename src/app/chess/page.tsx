b7762e6d4cec797787dfef1cac4e3f0a8a8389bd
Alex Kates <alex.kates@croissant.com>
2023-12-29 09:22:24 -0500

    Add @supabase/ssr package and import Auth component

--- src/app/chess/page.tsx
+++ src/app/chess/page.tsx
@@ -1,8 +1,12 @@
+import Auth from "@/components/auth";
 import Chessboard from "@/components/chessboard";
 
 function Page() {
   return (
     <div className="flex flex-col items-center">
+      <div className="flex w-full justify-end">
+        <Auth />
+      </div>
       <Chessboard />
     </div>
   );
---
+++ src/components/auth.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { createBrowserClient } from "@supabase/ssr";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+function Auth() {
+  const [email, setEmail] = useState<string | undefined>();
+  useEffect(() => {
+    async function init() {
+      const supabase = createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setEmail(user?.email);
+    }
+
+    init();
+  }, []);
+
+  async function signIn() {
+    const supabase = createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: "http://localhost:3000/auth/callback?next=/chess",
+      },
+    });
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+  }
+
+  async function signOut() {
+    const supabase = createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    setEmail(undefined);
+  }
+
+  if (email) {
+    return (
+      <div className="flex items-center text-sm">
+        <span>Welcome, {email}</span>
+        <Button variant={"link"} onClick={signOut}>
+          Sign out
+        </Button>
+      </div>
+    );
+  }
+
+  return <Button onClick={signIn}>Sign in</Button>;
+}
+
+export default Auth;
