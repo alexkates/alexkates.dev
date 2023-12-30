581a1ee7678d9bec83ab72a6af07cced0060e444
Alex Kates <alex.kates@croissant.com>
2023-12-30 13:58:35 -0500

    Refactor createServerClient function and add createServerActionClient function

--- src/lib/createSupabaseServerClient.ts
+++ src/lib/createSupabaseServerClient.ts
@@ -2,10 +2,24 @@ import { Database } from "@/types/supabase";
 import { CookieOptions, createServerClient as ssr } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export default function createServerClient() {
+export function createServerClient() {
   const cookieStore = cookies();
 
   const supabase = ssr<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+    cookies: {
+      get(name: string) {
+        return cookieStore.get(name)?.value;
+      },
+    },
+  });
+
+  return supabase;
+}
+
+export function createServerActionClient() {
+  const cookieStore = cookies();
+
+  const supabase = ssr<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY!, {
     cookies: {
       get(name: string) {
         return cookieStore.get(name)?.value;
@@ -14,7 +28,7 @@ export default function createServerClient() {
         cookieStore.set({ name, value, ...options });
       },
       remove(name: string, options: CookieOptions) {
-        cookieStore.delete({ name, ...options });
+        cookieStore.set({ name, value: "", ...options });
       },
     },
   });
