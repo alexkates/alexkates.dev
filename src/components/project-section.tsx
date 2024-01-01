a596fa1b6b25e4b39bc4cc08e9ebfeb4836e559d
Alex Kates <alex.kates@croissant.com>
2024-01-01 13:21:33 -0500

    Update project section text

--- src/components/project-section.tsx
+++ src/components/project-section.tsx
@@ -5,7 +5,7 @@ function ProjectSection() {
   return (
     <div className="mt-8 flex flex-col gap-2">
       <section className="prose prose-neutral dark:prose-invert">
-        <p>I&apos;m passionate about building new products. Here are a few I&apos;ve been a part of recently.</p>
+        <p>I&apos;m passionate about building products, especially from zero to one. Here are a few I&apos;ve been a part of recently.</p>
       </section>
       <ProjectList projects={projects} />
     </div>
