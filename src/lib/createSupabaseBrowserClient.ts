01da28c4bb3677cae48ebdb29c80b188eaf5efc4
Alex Kates <alex.kates@croissant.com>
2024-01-02 17:28:20 -0500

    Remove Supabase related code and dependencies

--- src/lib/createSupabaseBrowserClient.ts
+++
@@ -1,8 +0,0 @@
-import { Database } from "@/types/supabase";
-import { createBrowserClient as csr } from "@supabase/ssr";
-
-export default function createBrowserClient() {
-  const supabase = csr<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
-
-  return supabase;
-}
