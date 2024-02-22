6ec07801123c906367a8c17312b6e2b0d88e64f0
Alex Kates <alex.kates@croissant.com>
2024-02-22 05:41:48 -0500

    Refactor createClient function

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,4 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 import { Database } from "./types";
 
-export const createClient = () => createBrowserClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+export function createClient() {
+  const { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } = process.env;
+
+  return createBrowserClient<Database>(NEXT_PUBLIC_SUPABASE_URL!, NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+}
