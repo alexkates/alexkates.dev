7534c83fb24ead94598f5309eca566e2154bdf36
Alex Kates <alex.kates@croissant.com>
2024-02-22 05:46:45 -0500

    Refactor createClient function to use destructured environment variables

--- src/supabase/server.ts
+++ src/supabase/server.ts
@@ -3,7 +3,9 @@ import { type cookies } from "next/headers";
 import { Database } from "./types";
 
 export function createClient(cookieStore: ReturnType<typeof cookies>) {
-  return createServerClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+  const { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } = process.env;
+
+  return createServerClient<Database>(NEXT_PUBLIC_SUPABASE_URL!, NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
     cookies: {
       get(name: string) {
         return cookieStore.get(name)?.value;
@@ -11,20 +13,12 @@ export function createClient(cookieStore: ReturnType<typeof cookies>) {
       set(name: string, value: string, options: CookieOptions) {
         try {
           cookieStore.set({ name, value, ...options });
-        } catch (error) {
-          // The `set` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
-        }
+        } catch (error) {}
       },
       remove(name: string, options: CookieOptions) {
         try {
           cookieStore.set({ name, value: "", ...options });
-        } catch (error) {
-          // The `delete` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
-        }
+        } catch (error) {}
       },
     },
   });
