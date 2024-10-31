5cdbdf1c42c3f30040e2992e4872ea1cdfe12207
Alex Kates <alex.kates@croissant.com>
2024-10-31 06:24:44 -0400

    refactor /about social-list

--- src/components/social-list.tsx
+++ src/components/social-list.tsx
@@ -1,7 +1,10 @@
 import getUser from "@/server/get-user";
-import { GitHubLogoIcon, IdCardIcon, LinkedInLogoIcon, Pencil1Icon, StackIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon, LinkedInLogoIcon, Pencil1Icon, StackIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import { FileTextIcon } from "lucide-react";
 import Link from "next/link";
 import SocialListItem from "./social-list-item";
+
+const iconDimensions = { width: 14, height: 14 };
 
 async function SocialList() {
   const me = await getUser();
@@ -9,37 +12,37 @@ async function SocialList() {
   return (
     <ul className="flex flex-col justify-evenly">
       <SocialListItem>
-        <TwitterLogoIcon />
+        <TwitterLogoIcon {...iconDimensions} />
         <Link href={me?.socialMediaLinks?.twitter!} target="_blank">
           twitter
         </Link>
       </SocialListItem>
       <SocialListItem>
-        <GitHubLogoIcon />
+        <GitHubLogoIcon {...iconDimensions} />
         <Link href={me?.socialMediaLinks?.github!} target="_blank">
           github
         </Link>
       </SocialListItem>
       <SocialListItem>
-        <StackIcon />
+        <StackIcon {...iconDimensions} />
         <Link href={me?.socialMediaLinks?.stackoverflow!} target="_blank">
           stackoverflow
         </Link>
       </SocialListItem>
       <SocialListItem>
-        <LinkedInLogoIcon />
+        <LinkedInLogoIcon {...iconDimensions} />
         <Link href={me?.socialMediaLinks?.linkedin!} target="_blank">
           linkedin
         </Link>
       </SocialListItem>
       <SocialListItem>
-        <Pencil1Icon />
+        <Pencil1Icon {...iconDimensions} />
         <Link href="https://blog.alexkates.dev/" target="_blank">
           hashnode
         </Link>
       </SocialListItem>
       <SocialListItem>
-        <IdCardIcon />
+        <FileTextIcon {...iconDimensions} />
         <Link href="/resume.pdf" target="_blank">
           resume
         </Link>
